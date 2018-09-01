abf1165fcd8e7ea6a112c012c88f7cf9a61562e0
Hsuchiahui <a22923509@gmail.com>
2018-09-01 15:06:08 +0800

    set DB_URI

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
+import os
 
 # initialize
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
 app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', True)
 
 db = SQLAlchemy(app)
